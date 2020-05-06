30bd8de3b55e0cb6941ed6001277c66a276d0def
saanuregh <saanu09xs@gmail.com>
2020-05-06 20:39:27 +0530

    added districts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactMap from "./components/ReactMap";
 import Navbar from "./components/Navbar";
 import { useRoutes } from "hookrouter";
+import axios from "axios";
 
 const routes = {
-  "/": () => <ReactMap />,
+  '/': (stats) => <ReactMap stats={stats}/>,
 };
 
 function App() {
+  const [stats, setStats] = useState({});
+  const [fetched, setFetched] = useState(false);
   const routeResult = useRoutes(routes);
-  return (
+
+  useEffect(() => {
+    if (!fetched) {
+      (async () => {
+        let response = await axios.get(
+          "https://keralastats.coronasafe.live/latest.json"
+        );
+        let latest = response.data.summary;
+        response = await axios.get(
+          "https://keralastats.coronasafe.live/summary.json"
+        );
+        let summary = response.data.summary;
+        let lastUpdated = response.data.last_updated;
+        setStats({
+          latest: latest,
+          summary: summary,
+          lastUpdated: lastUpdated,
+        });
+        setFetched(true);
+      })();
+    }
+  }, [fetched]);
+
+  return fetched ? (
     <div className="relative h-screen w-screen font-inter">
       <div className="absolute inset-x-0 top-0 z-40">
         <Navbar />
       </div>
       {routeResult}
     </div>
+  ) : (
+    <div className="lds-dual-ring h-screen w-screen items-center justify-center overflow-hidden flex"></div>
   );
 }
 
--- src/components/ReactMap.js
+++ src/components/ReactMap.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
 import lsgdGeoData from "../assets/data/kerala_lsgd_compressed.json";
+import districtGeoData from "../assets/data/kerala_district.json";
 import Control from "react-leaflet-control";
 import { Map, GeoJSON, TileLayer, ZoomControl } from "react-leaflet";
 import L from "../assets/js/leaflet";
 import axios from "axios";
-
 const center = [10.551235, 76.034386];
 const bounds = L.latLngBounds(
   L.latLng(12.799159, 74.446244),
   L.latLng(8.209846, 77.623563)
 );
-const zoom = 8
+const zoom = 7.9;
 
-export default function MapContainer() {
+export default function MapContainer({ stats }) {
   const [fetched, setFetched] = useState(false);
+  const [districtOnly, setDistrictOnly] = useState(false);
   const [hotspots, setHotspots] = useState([]);
   const [zones, setZones] = useState({});
   const [state, setState] = useState({
@@ -41,36 +42,47 @@ export default function MapContainer() {
   }, [fetched]);
 
   const hotspotStyle = (feature) => {
+    let fillColor = zones[feature.properties.DISTRICT];
+    fillColor =
+      !districtOnly && hotspots.includes(feature.properties.LSGD)
+        ? "#800000"
+        : fillColor;
     return {
       color: "gray",
       weight: 0.5,
       fillOpacity: 0.5,
-      fillColor: hotspots.includes(feature.properties.LSGD)
-        ? "#800000"
-        : zones[feature.properties.DISTRICT],
+      fillColor: fillColor,
+    };
+  };
+
+  const districtStyle = (feature) => {
+    return {
+      color: "#C0C0C0",
+      weight: 1,
+      fillColor: "transparent",
     };
   };
 
   const renderTooltip = (feature) => {
-    return hotspots.includes(feature.properties.LSGD)
-      ? `${feature.properties.LSGD} <br/>
-          Zone: Containment`
-      : `${feature.properties.LSGD} <br/>
-            No Data`;
+    let content = `${feature.properties.LSGD}<br/>${feature.properties.DISTRICT}<br/>`;
+    let distZone = zones[feature.properties.DISTRICT];
+    hotspots.includes(feature.properties.LSGD)
+      ? (content += `Zone: Containment<br/>`)
+      : (content += `Zone: ${distZone}<br/>`);
+    return content;
   };
 
   const onEachFeature = (feature, layer) => {
-    const tooltipChildren = renderTooltip(feature);
-    const popupContent = `<Popup> ${tooltipChildren} </Popup>`;
-    layer.bindPopup(popupContent);
+    if (fetched) {
+      const tooltipChildren = renderTooltip(feature);
+      const popupContent = `<Popup> ${tooltipChildren} </Popup>`;
+      layer.bindPopup(popupContent);
+    }
   };
 
   const streetLabelsRenderer = new L.StreetLabels({
     collisionFlg: true,
     propertyName: "LSGD",
-    showLabelIf: function (layer) {
-      return true;
-    },
     fontStyle: {
       dynamicFontSize: true,
       fontSize: 10,
@@ -84,10 +96,10 @@ export default function MapContainer() {
     <Map
       center={state.center}
       // bounds={state.bounds}
-      minZoom={7}
+      minZoom={7.9}
       zoom={state.zoom}
       zoomControl={false}
-      zoomDelta={0.25}
+      zoomSnap={0.1}
       renderer={streetLabelsRenderer}
     >
       <TileLayer
@@ -100,9 +112,22 @@ export default function MapContainer() {
         style={hotspotStyle}
         onEachFeature={onEachFeature}
       />
+      <GeoJSON
+        data={districtGeoData}
+        style={districtStyle}
+      />
       <Control position="bottomright">
-        <button className="text-white" onClick={() => setState({ center: center, zoom: zoom })}>
+        <button
+          className="text-white"
+          onClick={() => setState({ center: center, zoom: zoom })}
+        >
           Reset View
+        </button>
+        <button
+          className="text-white"
+          onClick={() => setDistrictOnly(!districtOnly)}
+        >
+          District??
         </button>
       </Control>
     </Map>
